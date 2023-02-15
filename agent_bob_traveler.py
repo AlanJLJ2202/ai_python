7cd1b68590d1242ed1b87e902046e08f27009478
AlanJLJ2202 <alanjlj2202@gmail.com>
2023-02-15 09:17:38 -0600

    Agent traveler progress

---
+++ agent_bob_traveler.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+#Se declaran la cantidad de ubicaciones
+ubicaciones = 4
+
+#Se declara de donde inicia el recorrido del agente viajero
+bob_house = 2
+
+#Se declara la matriz de distancias
+distances = np.array(
+    ['1', '0', '7'],
+    ['2', '4', '3'],
+    ['3', '6', '1'],
+    ['4', '5', '1'],
+)
+
+
+
