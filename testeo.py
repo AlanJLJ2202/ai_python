933a852afd1df5b839942d6c2dfee74a1f749f34
AlanJLJ2202 <alanjlj2202@gmail.com>
2023-02-16 23:45:13 -0600

    Commit test from VSCode (#11)

--- testeo.py
+++ testeo.py
@@ -23,74 +23,116 @@ data = [
 df = pd.DataFrame(data, columns=['indice', 'x', 'y'])
 #print(df)
 
-
 # Mover las filas que coinciden con la condición a la primera posición
 df = pd.concat([df[df['indice'] == bob_house], df[df['indice'] != bob_house]]).reset_index(drop=True)
 
-# Mostrar el dataframe ordenado
-print(df)
-
-
-# Definir la función para calcular la distancia euclidiana entre dos puntos
-def euclidean_distance(point1, point2):
-    print('DISTANCIA EUCLIDIANA')
-    print('PUNTO 1', point1[1])
-    print('PUNTO 2', point2[1])
-
-    return np.sqrt((point1[1] - point2[1])**2 + (point1[2] - point2[2])**2)
-
-# Crear una lista vacía para almacenar la ruta recorrida por el agente
-route = []
-
-# Definir la primera posición del agente como el punto de inicio y agregarlo a la ruta
-start_point = df.iloc[0]
-route.append(start_point.indice)
-#print(start_point.indice)
-
-dist_matrix = squareform(pdist(df[['x', 'y']]))
-#print(dist_matrix)
-#print('lenght', len(dist_matrix))
-matriz_sin_ceros = np.ma.masked_equal(dist_matrix, 0)
-
-print(matriz_sin_ceros)
-sumatoria = 0
+#Se calcula la matriz de distancias
+matriz_distancias = squareform((pdist(df[['x', 'y']])))
 
 puntos_visitados = []
 indice_actual = 0
 
-#Distancia minima partiendo del punto 2
+for i in range(len(matriz_distancias)):
+     for j in range(len(matriz_distancias)):
+        if matriz_distancias[i, j] != '--' and matriz_distancias[i, j] != 0.0:
+            valor_nuevo = int(matriz_distancias[i, j] + 0.5)
+            matriz_distancias[i, j] = int(matriz_distancias[i, j] + 0.5)
+            
+            
+print(matriz_distancias)
 
-print('MINIMO', np.min(matriz_sin_ceros[indice_actual]))
-print('INDICE DEL VALOR MENOR', np.argmin(matriz_sin_ceros[indice_actual]))
+# Definimos el punto inicial y lo agregamos a una lista de puntos visitados
+punto_inicial = 0
+puntos_visitados = [punto_inicial]
 
-puntos_visitados.append(df.iloc[indice_actual].indice)
 
+# Definimos una función para encontrar el punto más cercano a un punto dado
+def encontrar_vecino_mas_cercano(point, puntos_visitados):
+
+    # Calculamos las distancias entre el punto y todos los demás puntos
+    distancia_a_todos = matriz_distancias[point, :]
+    print(distancia_a_todos)
+
+    # Eliminamos los puntos que ya fueron visitados
+    distancia_a_todos[puntos_visitados] = np.inf
+
+    # Encontramos el índice del punto más cercano
+    vecino_mas_cercano = np.argmin(distancia_a_todos)
+    return vecino_mas_cercano
+
+
+# Iteramos sobre todos los puntos de la matriz de distancias
+for i in range(len(matriz_distancias)):
+
+    print(puntos_visitados[-1])
+    # Encontramos el punto más cercano al último punto visitado
+    nearest_neighbor = encontrar_vecino_mas_cercano(puntos_visitados[-1], puntos_visitados)
+
+    # Agregamos el punto más cercano a la lista de puntos visitados
+    puntos_visitados.append(nearest_neighbor)
+
+
+puntos = []
+for point in puntos_visitados:
+    if df.loc[point].indice not in puntos:
+        puntos.append(df.loc[point].indice)
+
+
+print(puntos)
+print(puntos_visitados)
+
+# Definir la función para calcular la distancia euclidiana entre dos puntos
+def euclidean_distance(point1, point2):
+     return np.sqrt((point1[1] - point2[1])**2 + (point1[2] - point2[2])**2)
+
+# Calculamos la sumatoria de las distancias entre los puntos visitados
+sum_of_distances = 0
+for i in range(1, len(puntos_visitados)):
+    sum_of_distances += matriz_distancias[puntos_visitados[i-1], puntos_visitados[i]]
+
+
+#Calcular la distancia total recorrida por el agente
+total_distance = 0
+for i in range(len(puntos_visitados)-1):
+     total_distance += euclidean_distance(df.loc[puntos_visitados[i]], df.loc[puntos_visitados[i+1]])
+
+print("Sumatoria de distancias recorridas:", int(total_distance))
+
+''''
+for i in range(len(matriz_distancias)):
+    print('ITERACION', i)
+    print('INDICE ACTUAL', indice_actual)
+
+
+    #indice_actual = np.argmin(matriz_distancias[i])
+    #if df.loc[indice_actual].indice not in puntos_visitados:
+        #puntos_visitados.append(df.loc[indice_actual].indice)
+
+    print('-------------------------------------')
+    #indice_actual = np.argmin(matriz_distancias[i])
+
+
+    indice_actual = np.argmin(matriz_distancias[i])
+    if df.loc[indice_actual].indice not in puntos_visitados:
+        puntos_visitados.append(df.loc[indice_actual].indice)
+            
+            
+        #indice_actual = np.argmin(matriz_distancias[i])
+        #print('INDICE', df.loc[np.argmin(matriz_distancias[i])])
+
+     
+
+
+#print('SUMATORIA', sumatoria)
+print(matriz_distancias)
 print('PUNTOS VISITADOS', puntos_visitados)
 
 
-
-# for i in range(len(matriz_sin_ceros)):
-    
-#     print('i', i)
-#     for j in range(len(matriz_sin_ceros)):
-#         print(matriz_sin_ceros[i, j])
-
-#     print('MINIMO', np.min(matriz_sin_ceros[i]))
-#     sumatoria += np.min(matriz_sin_ceros[i])
-#     print('INDICE DEL VALOR MENOR', np.argmin(matriz_sin_ceros[i]))
-
-#     print('-----------------------')
-
-# print('SUMATORIA', sumatoria)
-
-
-
-
-
+'''
 
 # Mientras haya puntos sin visitar, buscar el punto más cercano al punto actual y agregarlo a la ruta
-#visited_points = set()
-#visited_points.add(start_point.indice)
+#puntos_visitados = set()
+#puntos_visitados.add(start_point.indice)
 # print(len(df))
 
 
