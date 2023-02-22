f4790c5bbb771a0efaa7c923b328160beef1d53f
AlanJLJ2202 <alanjlj2202@gmail.com>
2023-02-22 08:33:24 -0600

    New PSO algorithm numpy

--- pso_algorithm.py
+++ pso_algorithm.py
@@ -1,7 +1,7 @@
 import operator
 import random
 
-import numpy
+import numpy as np
 import math
 
 from deap import base
@@ -22,37 +22,51 @@ def generate(size, pmin, pmax, smin, smax):
 
 
 def updateParticle(part, best, phi1, phi2):
-    u1 = (random.uniform(0, phi1) for _ in range(len(part)))
-    u2 = (random.uniform(0, phi2) for _ in range(len(part)))
-    v_u1 = map(operator.mul, u1, map(operator.sub, part.best, part))
-    v_u2 = map(operator.mul, u2, map(operator.sub, best, part))
-    part.speed = list(map(operator.add, part.speed, map(operator.add, v_u1, v_u2)))
+
+    # u1 = (random.uniform(0, phi1) for _ in range(len(part)))
+    # u2 = (random.uniform(0, phi2) for _ in range(len(part)))
+    # v_u1 = map(operator.mul, u1, map(operator.sub, part.best, part))
+    # v_u2 = map(operator.mul, u2, map(operator.sub, best, part))
+
+    phi1 = np.random.uniform(0, phi1)
+    phi2 = np.random.uniform(0, phi2)
+
+    cognitive = phi1 * np.subtract(part.speed, part)
+    social = phi2 * np.subtract(best, part)
+    part.speed = part.speed + cognitive + social
+
+
+    #part.speed = list(map(operator.add, part.speed, map(operator.add, v_u1, v_u2)))
     for i, speed in enumerate(part.speed):
         if abs(speed) < part.smin:
             part.speed[i] = math.copysign(part.smin, speed)
         elif abs(speed) > part.smax:
             part.speed[i] = math.copysign(part.smax, speed)
-    part[:] = list(map(operator.add, part, part.speed))
+
+    #finalmente actualizamos la particula 
+    #part[:] = list(map(operator.add, part, part.speed))
+    part[:] = part + part.speed
 
 
 toolbox = base.Toolbox()
-toolbox.register("particle", generate, size=2, pmin=-6, pmax=6, smin=-3, smax=3)
+#pmin y pmax es de pocision
+toolbox.register("particle", generate, size=3, pmin=-5, pmax=5, smin=-3, smax=3)
 toolbox.register("population", tools.initRepeat, list, toolbox.particle)
 toolbox.register("update", updateParticle, phi1=2.0, phi2=2.0)
-toolbox.register("evaluate", benchmarks.h1)
+toolbox.register("evaluate", benchmarks.sphere)
 
 def main():
-    pop = toolbox.population(n=5)
+    pop = toolbox.population(n=100)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean)
-    stats.register("std", numpy.std)
-    stats.register("min", numpy.min)
-    stats.register("max", numpy.max)
+    stats.register("avg", np.mean)
+    stats.register("std", np.std)
+    stats.register("min", np.min)
+    stats.register("max", np.max)
 
     logbook = tools.Logbook()
     logbook.header = ["gen", "evals"] + stats.fields
 
-    GEN = 1000
+    GEN = 10000
     best = None
 
     for g in range(GEN):
