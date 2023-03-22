a74238bf306cfd2eca3ac75f31905efadfa67cc4
AlanJLJ2202 <alanjlj2202@gmail.com>
2023-03-22 17:23:31 -0600

    Clustering y KNN

--- KMEANS/clustering.py
+++ KMEANS/clustering.py
@@ -16,13 +16,12 @@ pixel_values = image.reshape((-1, 3))
 # convert to float
 pixel_values = np.float32(pixel_values)
 
-print(pixel_values.shape)
-
 # define stopping criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
 
 # number of clusters (K)
 k = 3
+
 _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 # convert back to 8 bit values  
